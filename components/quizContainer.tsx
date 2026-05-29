9794c8e6ca41aa105bde82dc827731d9f829f57b
haru <harutsuka0427@gmail.com>
2026-05-30 01:14:57 +0900

    ✨ feat: QuizContainerコンポーネントに選択肢表示機能と解答表示機能を追加

--- components/quizContainer.tsx
+++ components/quizContainer.tsx
@@ -1,14 +1,43 @@
+"use client";
 import style from "./quizContainer.module.css";
+import { useState } from "react";
 
 export default function QuizContainer({ question }: { question: any }) {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleShowAnswer = () => {
+    setShowAnswer((prev) => !prev);
+  };
+
   return (
     <div className={style.container}>
       {question && (
-        <div key={question.id} className={style.question}>
-          <p className={style.week}>第{question.week}回</p>
-          <p>問題: {question.question}</p>
-          {question.choices?.length > 0 && <p>選択肢: {question.choices}</p>}
-          <p>解答: {question.answer}</p>
+        <div key={question.id}>
+          <div className={style.question}>
+            <p className={style.week}>第{question.week}回</p>
+            <p>{question.question}</p>
+          </div>
+
+          <div className={style.choices}>
+            {question.choices?.length > 0 && (
+              <ul>
+                {question.choices.map((choice: string, index: number) => (
+                  <li key={index} className={style.choice}>
+                    {choice}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <button className={style.showAnswerButton} onClick={handleShowAnswer}>
+            解答を表示
+          </button>
+          {showAnswer && (
+            <div className={style.answer}>
+              <p>解答: {question.answer}</p>
+            </div>
+          )}
         </div>
       )}
     </div>
