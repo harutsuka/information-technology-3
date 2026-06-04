d45dfb12986aeb96cf0f75133c2094f7274a8e59
harutsuka <harutsuka0427@gmail.com>
2026-06-04 19:41:55 +0900

    🎨 refactor: Supabaseクライアントの関数名を変更し、エラーハンドリングを改善

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabase() {
+  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
+  console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
