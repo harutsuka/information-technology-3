962c180527186bd1336f54f7231102a79f3fd256
harutsuka <harutsuka0427@gmail.com>
2026-06-04 22:43:53 +0900

    ✨ feat: @vercel/analyticsを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/next";
 
 const notoSans = Noto_Sans_JP({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
           情報3 定着確認
         </Link>
         {children}
+        <Analytics />
       </body>
     </html>
   );
