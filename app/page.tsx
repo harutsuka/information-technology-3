25c37c02dc16ea8069aed1247813d33758d2f423
harutsuka <harutsuka0427@gmail.com>
2026-07-22 10:09:17 +0900

    💄 style: PCでの「クイズを始めるボタン」の位置を変更

--- app/page.tsx
+++ app/page.tsx
@@ -34,26 +34,27 @@ export default function Home() {
   return (
     <main className="container px-6 py-4 mx-auto md:pt-24 md:flex md:flex-col md:px-16">
       <div className="flex flex-col items-center gap-4 max-w-md mx-auto p-4 md:max-w-6xl md:flex-row md:items-center md:gap-16">
-        <div className="w-full text-center border border-dashed border-gray-300 p-6 rounded-lg mb-2 md:mb-0">
-          <RangeSettings
-            selectedWeeks={selectedWeeks}
-            setSelectedWeeks={setSelectedWeeks}
-            limit={limit}
-            setLimit={setLimit}
-            onlyFavorites={onlyFavorites}
-            setOnlyFavorites={setOnlyFavorites}
-            excludeMastered={excludeMastered}
-            setExcludeMastered={setExcludeMastered}
-          />
-        </div>
         <div className="flex flex-col gap-4 w-full">
+          <div className="w-full text-center border border-dashed border-gray-300 p-6 rounded-lg mb-2 md:mb-0">
+            <RangeSettings
+              selectedWeeks={selectedWeeks}
+              setSelectedWeeks={setSelectedWeeks}
+              limit={limit}
+              setLimit={setLimit}
+              onlyFavorites={onlyFavorites}
+              setOnlyFavorites={setOnlyFavorites}
+              excludeMastered={excludeMastered}
+              setExcludeMastered={setExcludeMastered}
+            />
+          </div>
           {/* 動的に作ったURL（quizUrl）を使ってクイズページに飛ぶ */}
           <Link href={quizUrl} className="w-full inline-block">
             <div className="bg-main-color hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-lg text-center shadow-main">
               🚀 クイズをはじめる
             </div>
           </Link>
-
+        </div>
+        <div className="flex flex-col gap-4 w-full">
           <Link href="/questions" className="w-full inline-block">
             <div className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-4 px-6 rounded-lg text-center shadow-main transition-colors">
               📕 問題一覧を見る
@@ -71,7 +72,9 @@ export default function Home() {
             </div>
           </Link>
           <div className="w-full inline-block bg-gray-100 border border-gray-300 text-gray-700 font-medium py-3 px-6 rounded-lg text-center">
-            <p>お気に入りの問題・覚えた問題でフィルターして出題できるようになりました！</p>
+            <p>
+              お気に入りの問題・覚えた問題でフィルターできるようになりました！
+            </p>
           </div>
         </div>
       </div>
