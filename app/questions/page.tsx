d58c3746daf871e95105929842a9f79aac6405c2
harutsuka <harutsuka0427@gmail.com>
2026-07-28 17:26:03 +0900

    🚑 fix: 問題データを更新するために、サーバーサイドレンダリングを強制するためのdynamicエクスポートを追加

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,6 +1,8 @@
 import { getQuestions } from "@/lib/questions";
 import QuizListAll from "./QuizListAll";
 
+export const dynamic = "force-dynamic"; // このページは常にサーバーサイドでレンダリングされるようにする
+
 export default async function QuestionsPage() {
   const allQuestions = (await getQuestions()) || [];
 
